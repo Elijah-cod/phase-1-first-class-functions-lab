dadddb2f1201aba7f855eab9f214f0599dc578da
Elijah <iamngunjiri@gmail.com>
2024-11-28 10:47:59 +0300

    Complete Advanced firstclass functions lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = (drivers) => {
+    return [drivers[0], drivers[1]]
+}
+
+const returnLastTwoDrivers = (drivers) => [drivers[2], drivers[3]]
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (number) {
+    return (fare) => fare * number
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, someFunction) => someFunction(drivers)
